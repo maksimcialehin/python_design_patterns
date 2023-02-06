e91cc94c7802e5e3f33d190f465668248aed786c
maksimcialehin <madqfrog1863@gmail.com>
2023-02-06 22:26:13 +0300

    Flyweight finishing

--- flyweight/exercise.py
+++ flyweight/exercise.py
@@ -1,12 +1,26 @@
 class Sentence:
     def __init__(self, plain_text):
         self.plain_text = plain_text.split()
+        self.formatting = {}
+
+    class Word:
+        def __init__(self, capitalize=False):
+            self.capitalize = capitalize
 
     def __getitem__(self, item):
-        return self.plain_text[item]
+        w = self.Word()
+        self.formatting[item] = w
+        return self.formatting[item]
 
-    def capitalize(self): return None
-
+    def __str__(self):
+        result = []
+        for i in range(len(self.plain_text)):
+            word = self.plain_text[i]
+            wf = self.formatting.get(i)
+            if wf and wf.capitalize:
+                word = word.upper()
+            result.append(word)
+        return ' '.join(result)
 
 
 if __name__ == '__main__':
